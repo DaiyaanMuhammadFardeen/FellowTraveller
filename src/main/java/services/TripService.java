ebee1d19a3998898a0db688aa6e236ebb07f193e
DaiyaanMuhammadFardeen <daiyaan.muhammad2002@gmail.com>
2024-12-04 11:16:53 +0600

    Added a service layer for the TripRepository

---
+++ src/main/java/services/TripService.java
@@ -0,0 +1,47 @@
+package com.dbmsproject.fellowtraveller.services;
+
+import com.dbmsproject.fellowtraveller.model.Trip;
+import com.dbmsproject.fellowtraveller.repositories.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TripService {
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    public List<Trip> getTripsOfUser(Long userId) {
+        return tripRepository.findByUserId(userId);
+    }
+
+    public Trip getTripById(Long id) {
+        Optional<Trip> optionalTrip = tripRepository.findById(id);
+        return optionalTrip.orElse(null);
+    }
+
+    public Trip createTrip(Trip trip) {
+        return tripRepository.save(trip);
+    }
+
+    public Trip updateTrip(Long id, Trip trip) {
+        Trip existingTrip = getTripById(id);
+        existingTrip.setStartLocation(trip.getStartLocation());
+        existingTrip.setDestination(trip.getDestination());
+        existingTrip.setStartDate(trip.getStartDate());
+        existingTrip.setEndDate(trip.getEndDate());
+        existingTrip.setUser(trip.getUser());
+        return tripRepository.save(existingTrip);
+    }
+
+    public void deleteTrip(Long id) {
+        tripRepository.deleteById(id);
+    }
+
+    public List<Trip> getAllTrips() {
+        return tripRepository.findAll();
+    }
+}
