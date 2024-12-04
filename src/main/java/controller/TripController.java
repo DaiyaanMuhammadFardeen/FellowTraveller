1b80462e69a0a12bf8d18c3dc67ab79a30b4200a
DaiyaanMuhammadFardeen <daiyaan.muhammad2002@gmail.com>
2024-12-04 11:15:25 +0600

    Added TripController to have API endpoints mapped

---
+++ src/main/java/controller/TripController.java
@@ -0,0 +1,50 @@
+package com.dbmsproject.fellowtraveller.controller;
+
+import com.dbmsproject.fellowtraveller.model.Trip;
+import com.dbmsproject.fellowtraveller.services.TripService;
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/trips")
+public class TripController {
+
+    @Autowired
+    private TripService tripService;
+
+    @GetMapping
+    public List<Trip> getAllTrips() {
+        return tripService.getAllTrips();
+    }
+
+    @GetMapping("/foruser/{userId}")
+    public List<Trip> getTripsOfUser(@PathVariable Long userId) {
+        return tripService.getTripsOfUser(userId);
+    }
+
+    @GetMapping("/{id}")
+    public Trip getTripById(@PathVariable Long id) {
+        return tripService.getTripById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Trip createTrip(@RequestBody Trip trip) {
+        return tripService.createTrip(trip);
+    }
+
+
+    @PutMapping("/{id}")
+    public Trip updateTrip(@PathVariable Long id, @RequestBody Trip updatedTrip) {
+        return tripService.updateTrip(id, updatedTrip);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTrip(@PathVariable Long id) {
+        tripService.deleteTrip(id);
+    }
+}
