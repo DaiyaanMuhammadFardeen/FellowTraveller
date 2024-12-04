95e93fd20161121d0b8b31eb837d6a18e9265b0f
DaiyaanMuhammadFardeen <daiyaan.muhammad2002@gmail.com>
2024-12-05 04:05:32 +0600

    Added Database interactivity through ItineraryRepository

---
+++ src/main/java/com/dbmsproject/fellowtraveller/repositories/ItineraryRepository.java
@@ -0,0 +1,11 @@
+package com.dbmsproject.fellowtraveller.repositories;
+
+import com.dbmsproject.fellowtraveller.models.Itinerary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+    List<Itinerary> findByUser_UserId(Long userId);
+}
