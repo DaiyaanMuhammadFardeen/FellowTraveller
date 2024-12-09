133a2c9dd358c072fa9007e5841d807568c2945e
DaiyaanMuhammadFardeen <daiyaan.muhammad2002@gmail.com>
2024-12-09 19:44:53 +0600

    Added a POST request to /api/destinations to handle destination creation with images

--- src/main/java/com/dbmsproject/fellowtraveller/controllers/DestinationController.java
+++ src/main/java/com/dbmsproject/fellowtraveller/controllers/DestinationController.java
@@ -4,12 +4,15 @@ import com.dbmsproject.fellowtraveller.models.Destination;
 import com.dbmsproject.fellowtraveller.models.Review;
 import com.dbmsproject.fellowtraveller.services.DestinationService;
 import com.dbmsproject.fellowtraveller.services.ReviewService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -35,5 +38,26 @@ public class DestinationController {
     @GetMapping("/{destinationId}/reviews")
     public List<Review> getReviewsByDestinationId(@RequestParam Long destinationId) {
         return reviewService.getReviewsByDestinationId(destinationId);
+    }
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Destination> createDestination(
+            @RequestPart("destination") String destinationJson,
+            @RequestPart("coverPhoto") MultipartFile coverPhoto) throws IOException {
+
+        // Parse JSON string into Destination object
+        ObjectMapper objectMapper = new ObjectMapper();
+        Destination destination = objectMapper.readValue(destinationJson, Destination.class);
+
+        // Convert coverPhoto to byte[] and set it in the destination
+        if (!coverPhoto.isEmpty()) {
+            destination.setCoverPhoto(coverPhoto.getBytes());
+        }
+
+        // Save destination
+        Destination createdDestination = destinationService.createDestination(destination);
+
+        // Return the created destination with location header
+        URI location = URI.create("/api/destinations/" + createdDestination.getDestinationId());
+        return ResponseEntity.created(location).body(createdDestination);
     }
 }
