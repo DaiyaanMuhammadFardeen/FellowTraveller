6fae0a5e4a32e63448d1519d5a08616598dbd530
DaiyaanMuhammadFardeen <daiyaan.muhammad2002@gmail.com>
2024-12-10 03:25:19 +0600

    URL controller to control pages and flow of application

---
+++ src/main/java/com/dbmsproject/fellowtraveller/controllers/URLController.java
@@ -0,0 +1,37 @@
+package com.dbmsproject.fellowtraveller.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class URLController {
+
+    @GetMapping("/")
+    public String index() {
+        return "forward:/index.html";  // Assuming you want index.html for the root URL
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "forward:/pages/login.html"; // Maps /login to /pages/login.html
+    }
+
+    @GetMapping("/settings")
+    public String userSettings() {
+        return "forward:/pages/usersettings.html"; // Maps /user-settings to /pages/usersettings.html
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "forward:/pages/home.html"; // Maps /home to /pages/home.html
+    }
+
+    @GetMapping("/destination-maker")
+    public String destinationMaker() {
+        return "forward:/pages/destinationMaker.html"; // Maps /destination-maker to /pages/destinationMaker.html
+    }
+    @GetMapping("/destination/{destinationId}")
+    public String destinationDetails() {
+        return "forward:/pages/destinationPage.html"; // Maps /destination/{destinationId} to /pages/destination.html
+    }
+}
